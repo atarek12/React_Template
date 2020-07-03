84ff39861b39bde0ab86512e6e351cca5b2c16f2
Ahmed Tarek <a7md.tare2@gmail.com>
2020-07-03 02:34:22 +0200

    Updates

---
+++ precache-manifest.0e0cf19afd507573853801d0f02df661.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "093bf708fab9cf259d7fbf000596e2b5",
+    "url": "/React_Template/index.html"
+  },
+  {
+    "revision": "be31a1c8a050857cabd5",
+    "url": "/React_Template/static/css/main.9a53ff25.chunk.css"
+  },
+  {
+    "revision": "508eb2043dbe572e39d1",
+    "url": "/React_Template/static/js/2.ab5d9f12.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_Template/static/js/2.ab5d9f12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be31a1c8a050857cabd5",
+    "url": "/React_Template/static/js/main.50f76c7d.chunk.js"
+  },
+  {
+    "revision": "1e70107a29e1430891fb",
+    "url": "/React_Template/static/js/runtime-main.9a2d0996.js"
+  },
+  {
+    "revision": "020c9eb834982825b30175d37a0d17d0",
+    "url": "/React_Template/static/media/about-bg.020c9eb8.jpg"
+  }
+]);
